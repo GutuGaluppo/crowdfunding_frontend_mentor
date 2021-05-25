37e21fdc357bd34a676d1b9a3168a38b8e0ea353
Gutu Galuppo <galuppodev@gmail.com>
2021-05-25 14:57:31 +0200

    created SubHeader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './index.css'
 import Navbar from './Navbar/Navbar'
+import SubHeader from './SubHeader/SubHeader'
 
 function App() {
-  return (
+	return (
 		<>
-		<Navbar/>
+			<Navbar />
+			<main>
+				<SubHeader />
+			</main>
 		</>
-  );
+	);
 }
 
 export default App;
---
+++ src/SubHeader/SubHeader.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./subHeader-style.css"
+
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const SubHeader = () => {
+	return (
+		<div className="sub-header">
+			<div className="master-logo">
+				<span>
+					M
+				</span>
+			</div>
+			<h2>Mastercraft Bamboo Monitor Riser</h2>
+			<p>A beautiful & handcrafted monitor stand to reduce neck and eye strain.</p>
+			<div className="call-to-action">
+				<button className="btn-back-project">Back this project</button>
+				<div className="bookmark-container">
+					<button className="btn-bookmark">
+						<FontAwesomeIcon icon={faBookmark} />
+					</button>
+					<span>Bookmarked</span>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default SubHeader
