e309283f370140574982cf44860a208bdac3b174
Gutu Galuppo <galuppodev@gmail.com>
2021-05-26 11:16:43 +0200

    fixed radio behavior

--- src/OptionsModal/OptionsModal.js
+++ src/OptionsModal/OptionsModal.js
@@ -2,37 +2,37 @@ import React from 'react'
 import './optionsModal-style.css'
 
 function CardModal({ data }) {
-	const [isChecked, setIsChecked] = React.useState(false)
+	const [isChecked, setIsChecked] = React.useState('')
 
 	return (
 		<>
 			{
-				data.map(card => {
-					return <div key={card.title} className={`card ${card.outOfStock ? "out-of-stock" : ''}`}>
+				data.map(option => {
+					return <div key={option.title} className={`card ${option.outOfStock ? "out-of-stock" : ''}`}>
 
 						<div className="card-modal-wrapper">
 							<input
 								type="radio"
-								value={card.title}
-								checked={isChecked}
-								onChange={() => setIsChecked(true)}
+								value={option.title}
+								checked={isChecked === option.title}
+								onChange={() => setIsChecked(option.title)}
 							/>
 
 							<div className="card-title-container">
-								<h3>{card.title}</h3>
-								{card.pledge &&
-									<p>{card.pledge}</p>
+								<h3>{option.title}</h3>
+								{option.pledge &&
+									<p>{option.pledge}</p>
 								}
 							</div>
-							{card.pledgeLeft &&
+							{option.pledgeLeft &&
 								<div className="card-modal-pledge-left">
-									<h3>{card.pledgeLeft}<span>left</span></h3>
+									<h3>{option.pledgeLeft}<span>left</span></h3>
 								</div>
 							}
 						</div>
 
-						<p>{card.description}</p>
-						{isChecked &&
+						<p>{option.description}</p>
+						{option.title === isChecked &&
 							<div className="pledge-input-container">
 								<p>Enter your pledge</p>
 								<div>
