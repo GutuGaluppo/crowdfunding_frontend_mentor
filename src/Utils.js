254624ce77a9882011ac6a51113fb977b39fc3fc
Gutu Galuppo <galuppodev@gmail.com>
2021-05-27 12:17:28 +0200

    created Utils

---
+++ src/Utils.js
@@ -0,0 +1,36 @@
+export const crowdfundData = [
+	{
+		title: 'Pledge with no reward',
+		description: "Choose to support us without a reward if you simply believe in our project. As a backer, you will be signed up to receive product updates via email.",
+		buttonTitle: 'Select Reward',
+		outOfStock: false,
+		pledgeAmount: 0
+	},
+	{
+		title: 'Bamboo Stand',
+		pledge: 'Pledge $25 or more',
+		description: "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you’ll be added to a special Backer member list.",
+		pledgeLeft: 101,
+		buttonTitle: 'Select Reward',
+		outOfStock: false,
+		pledgeAmount: 25
+	},
+	{
+		title: 'Black Edition Stand',
+		pledge: 'Pledge $75 or more',
+		description: "You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
+		pledgeLeft: 64,
+		buttonTitle: 'Select Reward',
+		outOfStock: false,
+		pledgeAmount: 75
+	},
+	{
+		title: 'Mahogany Special Edition',
+		pledge: 'Pledge $200 or more',
+		description: "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
+		pledgeLeft: 0,
+		buttonTitle: 'Out of stock',
+		outOfStock: true,
+		pledgeAmount: 200
+	},
+]
