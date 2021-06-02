f727e6e0c354d66304f90dec97a34c5f2892dc9e
Gutu Galuppo <galuppodev@gmail.com>
2021-06-02 16:16:47 +0200

    added SubHeader mobile style

--- src/Components/SubHeader/SubHeader.js
+++ src/Components/SubHeader/SubHeader.js
@@ -1,26 +1,25 @@
 import React from 'react'
 import "./subHeader-style.css"
 import useToggle from '../../useToggle'
-import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SubHeader = ({ setShowModal }) => {
 	const [bookmarked, setBookmarked] = useToggle()
 
 	return (
-		<section className="sub-header">
-			<div className="master-logo">
+		<section className="sub_header">
+			<div className="master_logo">
 				<span>
 					M
 				</span>
 			</div>
 			<h2>Mastercraft Bamboo Monitor Riser</h2>
 			<p>A beautiful & handcrafted monitor stand to reduce neck and eye strain.</p>
-			<div className="call-to-action">
-				<button className="btn-back-project" onClick={setShowModal}>Back this project</button>
-				<div className="bookmark-container">
-					<button className={`btn-bookmark ${bookmarked ? 'bookmarked' : ''}`} onClick={setBookmarked}>
-						<FontAwesomeIcon icon={faBookmark} />
+			<div className="call_to_action">
+				<button onClick={setShowModal}>Back this project</button>
+				<div className="bookmark_container">
+					<button className={`btn_bookmark ${bookmarked ? 'bookmarked' : ''}`} onClick={setBookmarked}>
+						<FontAwesomeIcon icon={['fas', 'bookmark']} />
 					</button>
 					<span className={`${bookmarked ? 'bookmarked-text' : ''}`}>
 						{bookmarked ? 'Bookmarked' : 'Bookmark'}
