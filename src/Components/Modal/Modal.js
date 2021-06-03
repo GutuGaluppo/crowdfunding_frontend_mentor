ccfa29357e6a3e9f6d321c6bcb0c287139c0fdcd
Gutu Galuppo <galuppodev@gmail.com>
2021-06-03 15:12:20 +0200

    mobile version modal header and style

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -7,16 +7,17 @@ function Modal({ setShowModal, handleBacked, setShowThanks }) {
 	return (
 		<div className="modal">
 			<section className="modal_main">
-				<div className="modal_close_btn">
-					<FontAwesomeIcon
-						icon={['far', 'times']}
-						onClick={() => setShowModal(false)}
-						className="icon"
-					/>
+				<div className="modal_header">
+					<div className="modal_close_btn">
+						<h1>Back this project</h1>
+						<FontAwesomeIcon
+							icon={['fas', 'times']}
+							onClick={() => setShowModal(false)}
+							className="icon"
+						/>
+					</div>
+					<p>Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?</p>
 				</div>
-				<h1>Back this project</h1>
-				<p>Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?</p>
-
 				<div>
 					<OptionsModal
 						data={crowdfundData}
